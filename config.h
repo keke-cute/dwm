44fb50f3f4f40525b7c4b5558baf4452daaab25b
Liu Shike <liushike1997@gmail.com>
2021-02-04 19:28:34 +0800

    change to gruvbox_material

--- config.h
+++ config.h
@@ -14,13 +14,13 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "SF Pro Display Font:size=14" };
+static const char *fonts[]          = { "SF Pro Display Font:size=20" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#fdf1c7";
+static const char col_gray2[]       = "#ebdbb2";
+static const char col_gray3[]       = "#3c3836";
+static const char col_gray4[]       = "#282828";
+static const char col_cyan[]        = "#fabd2f";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
